f62c829038913dedb69e6ab8c8f7b6bf15498fbe
Pranav Vashi <neobuddy89@gmail.com>
2024-12-03 01:50:30 +0530

    dolby: Move manifest declaration BoardConfig.mk
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- dolby/setup.mk
+++ dolby/setup.mk
@@ -6,9 +6,5 @@ CONFIG_PATH := hardware/motorola/dolby
 # AudioFX
 TARGET_EXCLUDES_AUDIOFX := true
 
-# HIDL
-DEVICE_MANIFEST_FILE += $(CONFIG_PATH)/hidl/manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(CONFIG_PATH)/hidl/framework_compatibility_matrix.xml
-
 # SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(CONFIG_PATH)/sepolicy/vendor
